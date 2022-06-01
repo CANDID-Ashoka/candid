4e5b867b4b268ae7d07e9bd24cbddcc4bc107d8b
Medini Chopra <medini.chopra_ug23@ashoka.edu.in>
2022-06-01 19:52:52 +0530

    added content

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -14,22 +14,18 @@ const home = () => {
         Social Network Analysis is an emerging area of interdisciplinary research that studies social structures and interactions online. Powered with recent advances in Deep Learning, visualization libraries and computing power, it is now possible to efficiently analyze large corpuses of internet data to identify trends and narratives in online communities. 
           <br></br>
           <br></br>
-          Understanding social media and online discourse is also crucially important now with the increased penetration of the internet in everyone’s daily lives. There is sufficient research to suggest that one’s online activities are a reflection of their civil and political activities in real life. An insight into online discourse therefore, also holds significant value in understanding the real world, public opinion and popular sentiment. 
+          Understanding social media and online discourse is also crucially important now with the increased penetration of the internet in everyone’s daily lives. There is sufficient research to suggest that one’s online activities are a reflection of their civil and political activities in real life. An insight into online discourse therefore, also holds significant value in understanding the real world, public opinion and popular sentiment.          
           </p>
-            <h1 style = {{fontSize:"10vw", color:"#071736", padding:"18vh 0 3vh 3vw"}}>what we do.</h1>          
+            <h1 style = {{fontSize:"10vw", color:"#071736", padding:"18vh 0 3vh 3vw"}}>what it is.</h1>          
             </div>
             <div style={{display:"flex", flexDirection:"column", textAlign:"left", justifyContent:'flex-start', alignItems: 'baseline', backgroundColor:"#b3cdfc"}}>
         <p style = {{padding: "12vh 5vw 0 20vw", textAlign:"right", fontSize:"1.5vw", fontFamily:"sans-serif"}}>
-        The CANDID team has built a ML-based pipeline for analyzing discourse around any recent event on Twitter. The said pipeline includes Named Entity Resolution (NER), Sentiment Analysis and Social Graph Analysis - all using State-of-the-Art Tools and Deep Learning Models (distillBERT etc). We plan to use this pipeline to efficiently analyze online discourse around all geopolitical events in the future.
+        The CANDID team works in the intersection of Computer Science and Society to analyse and understand various aspects of Social Media and Online Networks. Housed at Ashoka University, CANDID frequently works with Media houses on commentaries & timely studies on  contemporary issues of significance and global interest. CANDID members are experienced in Deep Learning, Data Analysis and Visualisation, Social Network Analysis and various other skills. 
           <br></br>
           <br></br>
-          In addition to this, we have the following ongoing projects: <br></br>
-          1. Geolocating sentiments surrounding the Russia-Ukraine discourse on Twitter (Commissioned by ThePrint) <br></br>
-          2. Propaganda, Narrative and Echo-Chambers: a Network-based analysis of the Russia-Ukraine discourse <br></br>
-          3. Efficient and effective Tweet sampling strategies around geopolitical events <br></br>
-          4. Twitter Corpus for Multilingual Sentiment Analysis <br></br>
+          CANDID is an active academic research group at its core. Beyond collaborations with Media houses and other organisations, CANDID also carries out novel and original research into various aspects of online networks and social media. 
         </p>
-            <h1 style = {{fontSize:"10vw", color:"#071736", padding:"18vh 0 3vh 3vw"}}>who we are.</h1>          
+            <h1 style = {{fontSize:"10vw", color:"#071736", padding:"18vh 0 3vh 3vw"}}>what we do.</h1>          
             </div>
     </>
   )
--- frontend/src/screens/Projects.js
+++ frontend/src/screens/Projects.js
@@ -2,11 +2,49 @@ import React from 'react'
 
 export default function Projects() {
   return (
+    <>
     <div style={{ padding: "56vh 0 3vh 3vw", display: "flex", flexDirection: "column", textAlign: "left", justifyContent: 'flex-start', alignItems: 'baseline', backgroundColor: "#dbe8ff" }}>
             <h2 style={{ fontSize: "10vw", color: "#071736" }}>projects.</h2>
             <h4 style = {{fontSize:"2vw", fontWeight:"300", color:"#071736", paddingTop:"1vh"}}>
             Read about our work on disinformation, reportage and other topics.
             </h4>
         </div>
+
+        <div style={{display:"flex", flexDirection:"column", textAlign:"left", justifyContent:'flex-start', alignItems: 'baseline', backgroundColor:"#c7dbff"}}>
+        <p style = {{padding: "12vh 5vw 0 20vw", textAlign:"right", fontSize:"1.5vw", fontFamily:"sans-serif"}}>
+        Team CANDID has developed a sophisticated pipeline for analysing internet data using State-of-the-Art Tools and Deep Learning Models (distillBERT etc) for Named Entity Resolution (NER), Sentiment Analysis and Social Graph Analysis. The software suite is under active development and will be released soon.
+          <br></br>
+          </p>
+            <h2 style = {{fontSize:"10vw", color:"#071736", padding:"18vh 0 3vh 3vw"}}>software development.</h2>          
+            </div>
+
+        <div style={{display:"flex", flexDirection:"column", textAlign:"left", justifyContent:'flex-start', alignItems: 'baseline', backgroundColor:"#b3cdfc"}}>
+        <p style = {{padding: "12vh 5vw 0 20vw", textAlign:"right", fontSize:"1.5vw", fontFamily:"sans-serif"}}>
+        CANDID has investigated various aspects of the ongoing conflict in Ukraine (2022) in collaboration with ThePrint. We perform various frequency studies, temporal distribution of tweeting activity and polarisation on a corpus of over 500,000 tweets around the conflict. Our findings are summarised in an article featured in ThePrint and The FinePrint (their premium offering).
+          <br></br>
+            </p>
+            <h2 style = {{fontSize:"10vw", color:"#071736", padding:"18vh 0 3vh 3vw"}}>Russia Ukraine information war.</h2>          
+            </div>
+
+            <div style={{display:"flex", flexDirection:"column", textAlign:"left", justifyContent:'flex-start', alignItems: 'baseline', backgroundColor:"#b3cdfc"}}>
+        <p style = {{padding: "12vh 5vw 0 20vw", textAlign:"right", fontSize:"1.5vw", fontFamily:"sans-serif"}}>
+        CANDID is currently studying various aspects of media reportage and the biases therein, with a focus on Socio-Political events in India.
+          <br></br>
+          <br></br>
+          -
+            </p>
+            <h2 style = {{fontSize:"10vw", color:"#071736", padding:"18vh 0 3vh 3vw"}}>media biases and predictability.</h2>          
+            </div>
+
+            <div style={{display:"flex", flexDirection:"column", textAlign:"left", justifyContent:'flex-start', alignItems: 'baseline', backgroundColor:"#b3cdfc"}}>
+        <p style = {{padding: "12vh 5vw 0 20vw", textAlign:"right", fontSize:"1.5vw", fontFamily:"sans-serif"}}>
+        Team CANDID often publishes shorter-format blog articles which are intended to share short and crisp insights on relevant issues of public interest. Keep an eye out for these on our Blog page.
+          <br></br>
+          <br></br>
+          -
+            </p>
+            <h2 style = {{fontSize:"10vw", color:"#071736", padding:"18vh 0 3vh 3vw"}}>blog articles.</h2>          
+            </div>
+    </>
   )
 }
